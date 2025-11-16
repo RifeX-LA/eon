9c560dc815c6893b313d867410311637d0c17a54
RifeX-LA <timur322132@mail.ru>
2025-11-16 23:15:24 +0300

    add eon::unwrap_ref() and eon::unwrap_ptr()

--- utility.hpp
+++ utility.hpp
@@ -3,6 +3,8 @@
 #include <functional>
 #include <type_traits>
 
+#include <eon/concepts.hpp>
+
 namespace eon {
 
     template <typename T>
@@ -25,6 +27,34 @@ namespace eon {
         }
         else {
             return std::ref(fn);
+        }
+    }
+
+    /**
+     * @return stored value, if <b>T</b> is a specialization of <b>std::reference_wrapper</b>
+     * @return <b>std::forward<T>(value)</b> otherwise
+     */
+    template <typename T>
+    [[nodiscard]] constexpr decltype(auto) unwrap_ref(T && value) noexcept {
+        if constexpr (specialization_of<T, std::reference_wrapper>) {
+            return value.get();
+        }
+        else {
+            return std::forward<T>(value);
+        }
+    }
+
+    /**
+     * @return stored value, if <b>T</b> is a <i>pointer type</i>
+     * @return <b>std::forward<T>(value)</b> otherwise
+     */
+    template <typename T>
+    [[nodiscard]] constexpr decltype(auto) unwrap_ptr(T && value) noexcept {
+        if constexpr (std::is_pointer_v<std::remove_cvref_t<T>>) {
+            return *value;
+        }
+        else {
+            return std::forward<T>(value);
         }
     }
 
--- utility/README.md
+++ utility/README.md
@@ -3,7 +3,9 @@
 Features:
 
 * `eon::pass_fn()` - convenient way to pass functions
+* `eon::unwrap_ref<T>()` - returns stored value, if `T` is a `std::reference_wrapper`, or value itself otherwise
+* `eon::unwrap_ptr<T>()` - returns stored value, if `T` is a _pointer type_, or value itself otherwise
 
 # Requirements
 
-C++11
+C++20
